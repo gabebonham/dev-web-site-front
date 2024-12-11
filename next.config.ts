671f604ba55f69899a8528793cfe52c61bf9af6b
gabebonham <grotegabriel@gmail.com>
2024-12-11 16:08:23 -0300

    more configdss

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-	basePath: '/',
+	basePath: '/home',
 	env: {
 		BACKEND_URL:
 			'https://dev-web-site-back-production.up.railway.app/api',
