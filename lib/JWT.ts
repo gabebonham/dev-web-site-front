53da900ff1576841b88e42f3008236b4d956aec3
gabebonham <grotegabriel@gmail.com>
2024-12-14 22:28:04 -0300

    ç

--- lib/JWT.ts
+++ lib/JWT.ts
@@ -1,10 +1,10 @@
 import { jwtVerify } from 'jose';
-import 'dotenv';
-const o = process.env.JWT_KEY as string;
-console.log(o);
-const key = new TextEncoder().encode(o);
-console.log(key);
+
 export async function decrypt(session) {
+	const o = process.env.JWT_KEY as string;
+	console.log(o);
+	const key = new TextEncoder().encode(o);
+	console.log(key);
 	const { payload } = await jwtVerify(session.value, key, {
 		algorithms: ['HS256'],
 	});
