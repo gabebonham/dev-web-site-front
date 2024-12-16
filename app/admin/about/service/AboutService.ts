e760a48a244a003c903c73fa79d0f32a802ba247
gabebonham <grotegabriel@gmail.com>
2024-12-16 19:15:50 -0300

    dgh

--- app/admin/about/service/AboutService.ts
+++ app/admin/about/service/AboutService.ts
@@ -6,6 +6,8 @@ export async function getAbout(): Promise<About> {
 			await fetch(process.env.BACKEND_URL + '/about', {
 				headers: {
 					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin':
+						'https://dev-web-site-back-production.up.railway.app',
 				},
 				method: 'GET',
 				credentials: 'include',
@@ -22,6 +24,8 @@ export async function updateAbout(newAbout) {
 		const a = await fetch(process.env.BACKEND_URL + '/about', {
 			headers: {
 				'Content-Type': 'application/json',
+				'Access-Control-Allow-Origin':
+					'https://dev-web-site-back-production.up.railway.app',
 			},
 			method: 'PUT',
 			body: JSON.stringify({ newAbout: newAbout }),
--- app/sobre/_services/AboutService.ts
+++ app/sobre/_services/AboutService.ts
@@ -5,6 +5,8 @@ export async function getAbout() {
 			await fetch(process.env.BACKEND_URL + '/about', {
 				headers: {
 					'Content-Type': 'application/json',
+					'Access-Control-Allow-Origin':
+						'https://dev-web-site-back-production.up.railway.app',
 				},
 				method: 'GET',
 				credentials: 'include',
