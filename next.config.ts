31c25625c23b61e7a0f641519a3241270712e630
gabebonham <grotegabriel@gmail.com>
2024-12-16 03:23:09 -0300

    oij

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
 	staticPageGenerationTimeout: 10000,
 	env: {
-		BACKEND_URL: 'back:3000/api',
-		BACKEND_URL_PURE: 'back:3000',
+		BACKEND_URL:
+			'dev-web-site-back-production.up.railway.app:3000/api',
+		BACKEND_URL_PURE:
+			'dev-web-site-back-production.up.railway.app:3000',
 		JWT_KEY: 'grote',
 	},
 };
