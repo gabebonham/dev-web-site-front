4edf2d15c806f554620e59a04619da1d62249a5a
gabebonham <grotegabriel@gmail.com>
2024-12-16 00:33:58 -0300

    gaas

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	staticPageGenerationTimeout: 10000,
+	env: {
+		BACKEND_URL: 'http://back:3000/api',
+		BACKEND_URL_PURE: 'http://back:3000',
+		JWT_KEY: 'grote',
+	},
 };
 
 export default nextConfig;
