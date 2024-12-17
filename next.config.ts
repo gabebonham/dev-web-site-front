9b11e6a8c431012bfeafd2803e1c406b92384288
gabebonham <grotegabriel@gmail.com>
2024-12-17 05:08:16 -0300

    aggs

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,28 @@ const nextConfig: NextConfig = {
 			'https://dev-web-site-back-production.up.railway.app',
 		JWT_KEY: 'grote',
 	},
+	async headers() {
+		return [
+			{
+				// matching all API routes
+				source: '/:path*',
+				headers: [
+					{
+						key: 'Access-Control-Allow-Origin',
+						value: '*',
+					}, // replace this your actual origin
+					{
+						key: 'Access-Control-Allow-Methods',
+						value: 'GET,DELETE,PATCH,POST,PUT, OPTIONS',
+					},
+					{
+						key: 'Access-Control-Allow-Headers',
+						value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+					},
+				],
+			},
+		];
+	},
 };
 
 export default nextConfig;
