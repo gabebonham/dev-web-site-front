341c48ae8b90ceaecf9f9d44eb308a7360aa070d
gabebonham <grotegabriel@gmail.com>
2024-12-21 18:09:33 -0300

    fa

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,14 @@ import { decrypt } from './lib/JWT'; // Assuming decrypt is your custom decrypti
 import { headers } from 'next/headers';
 
 // Middleware to check for a valid session cookie
-export async function middleware(req, res: NextResponse) {
+export async function middleware(req, res) {
 	// Get the 'session' cookie from the request
 
 	const session = await req.cookies.get('session');
 	const decryptedData = await decrypt(session); // Assuming decrypt works here
 	const requestHeaders = new Headers(req.headers);
 
+	// res.setHeader('Set-Cookie', 'authorization=' + session.value);
 	requestHeaders.append(
 		'Access-Control-Allow-Origin',
 		'https://dev-web-site-back-production.up.railway.app',
@@ -23,21 +24,19 @@ export async function middleware(req, res: NextResponse) {
 		'Access-Control-Allow-Headers',
 		'Content-Type, Access-Control-Allow-Methods, authorization',
 	);
-	requestHeaders.append('Authorization', session.value);
+	requestHeaders.append('Set-Cookie', 'authorization=' + session.value);
 
 	requestHeaders.append('Accept', 'application/json');
 
-	console.log(req.headers);
+	// Set a new response header `x-hello-from-middleware2`
+	requestHeaders.set('authorization', session.value);
 	const response = NextResponse.next({
 		request: {
 			// New request headers
 			headers: requestHeaders,
 		},
 	});
-
-	// Set a new response header `x-hello-from-middleware2`
-	response.headers.set('authorization', session.value);
-
+	console.log(response);
 	if (decryptedData) {
 		return response;
 	} else {
