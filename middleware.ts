584fc78d8ed5bada4a64ecc34a6204ed26e68123
gabebonham <grotegabriel@gmail.com>
2024-12-22 01:48:58 -0300

    fa

--- middleware.ts
+++ middleware.ts
@@ -3,37 +3,39 @@ import { decrypt } from './lib/JWT'; // Assuming decrypt is your custom decrypti
 import { cookies, headers } from 'next/headers';
 
 // Middleware to check for a valid session cookie
-export async function middleware(req, res) {
+export async function middleware(req, res, next) {
 	// Get the 'session' cookie from the request
 
 	const session = await req.cookies.get('Authorization');
 	const decryptedData = await decrypt(session); // Assuming decrypt works here
-
 	let headerss = new Headers();
 
-	headerss.set('Authentication', session.value);
-	headerss.set('Accept', 'application/json');
-	headerss.set(
+	res.setHeaders('Authentication', session.value);
+	res.setHeaders('Accept', 'application/json');
+	res.setHeaders(
 		'Access-Control-Allow-Headers',
 		'Content-Type, Access-Control-Allow-Methods, Accept, Authorization, cookie, Set-Cookie',
 	);
-	headerss.set('Access-Control-Allow-Credentials', 'true');
-	headerss.set(
+	res.setHeaders('Access-Control-Allow-Credentials', 'true');
+	res.setHeaders(
 		'Access-Control-Allow-Methods',
 		'GET, POST, PUT, DELETE, OPTIONS,PATCH',
 	);
-	headerss.set(
+	res.setHeaders(
 		'Access-Control-Allow-Origin',
 		'https://dev-web-site-back-production.up.railway.app',
 	);
-	headerss.set('Access-Control-Expose-Headers', 'Authentication, cookie');
+	res.setHeaders(
+		'Access-Control-Expose-Headers',
+		'Authentication, cookie',
+	);
 	const response = NextResponse.next({
 		headers: headerss,
 	});
 	(await response.cookies).set('Authorization', session.value);
 	console.log(response);
 	if (decryptedData) {
-		return response;
+		return next();
 	} else {
 		return NextResponse.redirect(new URL('/home', req.url));
 	}
